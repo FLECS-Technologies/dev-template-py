ffcfae2b2ca0bece7e8a1ccb7d2fa1ea1b62fcf1
Lukas Simon <lukas@flecs.tech>
2025-07-16 15:35:06 +0200

    Fix code style problems

--- src/dev_template_py/server.py
+++ src/dev_template_py/server.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from dev_template_py.example import add_numbers
+"""A small example server that handles post requests on /add."""
 import logging
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from dev_template_py.example import add_numbers
 
 class BelowErrorFilter(logging.Filter):
+    """A logging filter that only allows loggings below level ERROR."""
+
     def filter(self, record):
+        """Filter everything with loggings below level ERROR."""
         return record.levelno < logging.ERROR
 
 
@@ -13,6 +18,8 @@ logger = logging.getLogger(__name__)
 
 
 class AddParameters(BaseModel):
+    """The parameters required in the body on /add."""
+
     left: float
     right: float
 
@@ -21,7 +28,8 @@ app = FastAPI(title="MyApp API", version="1.0")
 
 
 @app.post("/add")
-async def perform_action(params: AddParameters):
+async def handle_post_add(params: AddParameters):
+    """Handles a request on /add by adding the two number from the body."""
     logger.error("/add endpoint accessed")
     result = add_numbers(params.left, params.right)
     return {"result": result}
